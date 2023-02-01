319fd6f5d8db56080930ba4a8f8aa77b954317b4
Кильдюшевская Д.И <115226170+playwrightHell@users.noreply.github.com>
2023-02-01 20:14:29 +0300

    Лабораторная 3

---
+++ task1.py
@@ -0,0 +1,55 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        if isinstance(pages, int):
+            if pages > 0:
+                self.pages = pages
+            else:
+                raise AttributeError(f'error count of pages')
+        else:
+            raise AttributeError(f'error type of pages')
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Количество страниц {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        if isinstance(duration, float):
+            if duration > 0:
+                self.duration = duration
+            else:
+                raise AttributeError(f'error count of duration')
+        else:
+            raise AttributeError(f'error type of duration')
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}. Продолжительность {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
+
+
+b_1 = Book("dkfl", "lfkfds")
+print(b_1)
+b_2 = PaperBook("klsfdjlks", "sldkfljs", 234)
+print(b_2)
+b_3 = AudioBook("skjfdls", "skjhf", 23.8)
+print(b_3)
